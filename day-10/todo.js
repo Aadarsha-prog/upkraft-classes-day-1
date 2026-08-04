8e4e67035ced6551b59c2cd63268b20391fd32cb
suparth <suparthnarayanghimire2014@gmail.com>
2026-08-04 08:23:11 +0545

    checkbox in todo

--- day-10/todo.js
+++ day-10/todo.js
@@ -5,6 +5,8 @@ const clearTodosButton = document.querySelector("#clear-todos");
 
 const localTodos = localStorage.getItem("todos");
 
+// Array of string => Array of objects
+// abc, def, => {text: abc, completed: false}, {text: def, completed: true}
 let todos = [];
 
 try {
@@ -33,9 +35,15 @@ form.addEventListener("submit", (event) => {
     return;
   }
 
-  createNewTodoInUI(inputValue);
+  const newTodoItem = {
+    id: (todos.length + 1).toString(),
+    text: inputValue,
+    completed: false,
+  };
 
-  todos.push(inputValue);
+  createNewTodoInUI(newTodoItem);
+
+  todos.push(newTodoItem);
 
   localStorage.setItem("todos", JSON.stringify(todos));
 
@@ -48,11 +56,81 @@ clearTodosButton.addEventListener("click", () => {
   todoList.innerHTML = ""; // Clear the UI
 });
 
+// The arg todo, is object and not a string
 function createNewTodoInUI(todo) {
   const li = document.createElement("li");
   const p = document.createElement("p");
-  p.textContent = todo;
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
 
-  li.appendChild(p);
+  p.textContent = todo.text;
+
+  li.setAttribute("data-id", todo.id);
+
+  const div = document.createElement("div");
+
+  div.style.display = "flex";
+  div.style.alignItems = "center";
+  div.style.marginBottom = "10px";
+  div.style.listStyleType = "disc";
+
+  updatePUI(todo, p);
+
+  div.appendChild(p);
+  div.appendChild(checkbox);
+  li.appendChild(div);
   todoList.appendChild(li);
+}
+
+// Find all checkboxes and add event listener
+// But doing this will create multiple closures
+// const checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+// checkboxes.forEach((checkbox, index) => {
+//   checkbox.addEventListener("change", () => {
+//     todos[index].completed = checkbox.checked;
+//     localStorage.setItem("todos", JSON.stringify(todos));
+//   });
+// });
+
+todoList.addEventListener("change", (event) => {
+  const closestCheckbox = event.target.closest("input[type='checkbox']");
+  const closestLi = event.target.closest("li");
+  const closestP = event.target.closest("p");
+
+  const liDataId = closestLi.getAttribute("data-id");
+  console.log("liDataId", liDataId);
+
+  if (!closestCheckbox || !closestLi) {
+    return;
+  }
+
+  // Mark the todo item as completed or not completed
+  const isCompleted = closestCheckbox.checked;
+
+  // Find the index of todo in the todos array
+  const todoIndex = todos.findIndex((todo) => todo.id === liDataId);
+  console.log("todoIndex", todoIndex);
+
+  if (todoIndex === -1) {
+    return;
+  }
+
+  const todoItem = todos[todoIndex];
+  todoItem.completed = isCompleted;
+
+  console.log("todoItem", todoItem);
+  const p = document.querySelector(`li[data-id='${todoItem.id}'] p`);
+  updatePUI(todoItem, p);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+});
+
+function updatePUI(todo, p) {
+  if (todo.completed) {
+    p.style.textDecoration = "line-through";
+  } else {
+    p.style.textDecoration = "none";
+  }
 }
